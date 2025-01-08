e75334108b5f41efce11fbf94af2732c7f54a1b1
Daniel Brotsky <dan@clickonetwo.io>
2025-01-08 14:45:39 -0800

    Update copyrights to 2024-2025.

--- airtable/script-actions/link-asylum-questionnaire-extension.js
+++ airtable/script-actions/link-asylum-questionnaire-extension.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 Daniel C. Brotsky. All rights reserved.
+ * Copyright 2024-2025 Daniel C. Brotsky. All rights reserved.
  * All the copyrighted work in this repository is licensed under the
  * open source MIT License, reproduced in the LICENSE file.
  */
