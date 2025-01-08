5568240beb49c084ab55528172b56e9497eafab2
Daniel Brotsky <dan@clickonetwo.io>
2025-01-08 14:11:33 -0800

    Fix bug in intl phone number formatting.
    The process of adding hyphens was removing the last digits and not inserting hyphens in all places.

--- airtable/script-actions/phone.js
+++ airtable/script-actions/phone.js
@@ -33,7 +33,7 @@ function formatPhone(phone) {
         suffix = suffix.substring(0, suffix.length - 4);
     }
     for (let i = suffix.length; i > 3; i = i - 3) {
-        suffix = suffix.substring(0, i) + "-" + suffix.substring(i-3, i)
+        suffix = suffix.substring(0, i-3) + "-" + suffix.substring(i-3, i)
     }
     return prefix + "-" + suffix + suffixSuffix;
 }
