190fd8e8eb4a86128ce54dca05f87ba6f914050c
Daniel Brotsky <dan@clickonetwo.io>
2025-12-05 18:29:26 -0800

    Update index

--- airtable/scripts/development/index.ts
+++ airtable/scripts/development/index.ts
@@ -1,6 +1,8 @@
+// noinspection JSUnusedLocalSymbols
+
 import fs from "fs";
 import { fetchAllGbCampaigns, fetchAllGbPlans, fetchAllGbTransactions } from "./fetchGbDataLocally";
-import { GbTransactionData } from "./payloads";
+import { GbCampaignData, GbPlanData, GbTransactionData } from "./payloads";
 
 async function fetchGbDonations() {
     let path = "../../local/donations.json";
@@ -26,7 +28,7 @@ async function fetchGbPlans() {
         console.log(`Wrote ${plans.length} plans to local plans.json file.`);
         return plans;
     }
-    let plans: GbTransactionData[] = JSON.parse(fs.readFileSync(path, "utf8"));
+    let plans: GbPlanData[] = JSON.parse(fs.readFileSync(path, "utf8"));
     return plans;
 }
 
@@ -40,50 +42,52 @@ async function fetchGbCampaigns() {
         console.log(`Wrote ${campaigns.length} campaigns to local campaigns.json file.`);
         return campaigns;
     }
-    let campaigns: GbTransactionData[] = JSON.parse(fs.readFileSync(path, "utf8"));
+    let campaigns: GbCampaignData[] = JSON.parse(fs.readFileSync(path, "utf8"));
     return campaigns;
 }
 
-// async function findDonationsWithMismatchedAmounts() {
-//     const donations = await fetchGbDonations();
-//     const mismatches = donations.filter((d) => d.amount !== d.donated);
-//     const mismatchesCoveredFee = mismatches.filter((d) => d.fee_covered == d.fee);
-//     const ms = mismatches.map((d) => ({
-//         id: d.id,
-//         amount: d.amount,
-//         fee: d.fee,
-//         fee_covered: d.fee_covered,
-//         donated: d.donated,
-//         payout: d.payout,
-//     }));
-//     const msCoveredFee = mismatchesCoveredFee.map((d) => ({
-//         id: d.id,
-//         amount: d.amount,
-//         donated: d.donated,
-//         payout: d.payout,
-//     }));
-//     return { ms, msCoveredFee };
-// }
+async function findDonationsWithMismatchedAmounts() {
+    const donations = await fetchGbDonations();
+    const mismatches = donations.filter((d) => d.amount !== d.donated);
+    const mismatchesCoveredFee = mismatches.filter((d) => d.fee_covered == d.fee);
+    const ms = mismatches.map((d) => ({
+        id: d.id,
+        amount: d.amount,
+        fee: d.fee,
+        fee_covered: d.fee_covered,
+        donated: d.donated,
+        payout: d.payout,
+    }));
+    const msCoveredFee = mismatchesCoveredFee.map((d) => ({
+        id: d.id,
+        amount: d.amount,
+        donated: d.donated,
+        payout: d.payout,
+    }));
+    return { ms, msCoveredFee };
+}
 
-// async function findGbCustomFieldTitles() {
-//     const donations = await fetchGbDonations();
-//     const customFields = donations
-//         .filter((d) => d.custom_fields.length)
-//         .map((d) => d.custom_fields);
-//     const titles: string[] = [];
-//     for (const fields of customFields) {
-//         for (const field of fields) {
-//             if (!titles.includes(field.title)) {
-//                 titles.push(field.title);
-//             }
-//         }
-//     }
-//     return titles;
-// }
+async function findGbCustomFieldTitles() {
+    const donations = await fetchGbDonations();
+    const customFields = donations
+        .filter((d) => d.custom_fields.length)
+        .map((d) => d.custom_fields);
+    const titles: string[] = [];
+    for (const fields of customFields) {
+        for (const field of fields) {
+            if (!titles.includes(field.title)) {
+                titles.push(field.title);
+            }
+        }
+    }
+    return titles;
+}
 
 function main() {
-    fetchGbCampaigns().then((plans) => {
-        console.log(`Fetched ${plans.length} campaigns.`);
+    fetchGbCampaigns().then((campaigns) => {
+        for (const c of campaigns) {
+            console.log(`${c.id}, ${c.code}, ${c.title}`);
+        }
     });
 }
 
