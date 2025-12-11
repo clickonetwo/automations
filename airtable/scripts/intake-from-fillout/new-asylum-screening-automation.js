8712f690aa0dc37114a288b651b9c5644341c9ae
Daniel Brotsky <dan@clickonetwo.io>
2025-12-11 09:16:25 -0800

    Fix warnings and one logging error in Fillout forms.

--- airtable/scripts/intake-from-fillout/new-asylum-screening-automation.js
+++ airtable/scripts/intake-from-fillout/new-asylum-screening-automation.js
@@ -4,166 +4,189 @@
  * open source MIT License, reproduced in the LICENSE file.
  */
 
+import { base, input } from "../library/airtable.internal";
+
 // Global definitions
+// noinspection SpellCheckingInspection
 const masterNamedFieldMap = {
-    conflicts: "fldQky7MN4M6Qjqak",             // Additional Asylum Screening Submission Info
-    asylumLinks: "fldTRnUG4ESOqteAg",           // Asylum Screening Form
-    link: "fldumcuGachcsbTsl",                  // Asylum Screening Link
-    linkId: "fldBGO1zn0kcOYNEd",                // Asylum Screening Link ID
-}
+    conflicts: "fldQky7MN4M6Qjqak", // Additional Asylum Screening Submission Info
+    asylumLinks: "fldTRnUG4ESOqteAg", // Asylum Screening Form
+    link: "fldumcuGachcsbTsl", // Asylum Screening Link
+    linkId: "fldBGO1zn0kcOYNEd", // Asylum Screening Link ID
+};
 
+// noinspection SpellCheckingInspection
 const formNamedFieldMap = {
-    masterLink: "fld9j9o0qFtBczkjQ",            // Contact
-    createdDate: "fldCqHWHyKZdLjZBQ",           // Created
-}
+    masterLink: "fld9j9o0qFtBczkjQ", // Contact
+    createdDate: "fldCqHWHyKZdLjZBQ", // Created
+};
 
-const stringFieldMap = {  // map from field IDs in Asylum Screening Form table to All Contacts table
-    "fldgrzsoSDC41zvUs": "fldG8MGdqTySJhHJ4",   // Preferred Name -> Preferred Name
-    "fldlQlkYznc9z9uId": "flddEBS3pVsatT6Zh",   // A Number -> A Number
-    "fldrCDTE7PYGibJOR": "fld1NoEE1EB3ckLFW",   // Questions/Additional Info -> Questions (Asylum Screening)
-}
+// noinspection SpellCheckingInspection
+const stringFieldMap = {
+    // map from field IDs in Asylum Screening Form table to All Contacts table
+    fldgrzsoSDC41zvUs: "fldG8MGdqTySJhHJ4", // Preferred Name -> Preferred Name
+    fldlQlkYznc9z9uId: "flddEBS3pVsatT6Zh", // A Number -> A Number
+    fldrCDTE7PYGibJOR: "fld1NoEE1EB3ckLFW", // Questions/Additional Info -> Questions (Asylum Screening)
+};
 
+// noinspection SpellCheckingInspection
 const dateFieldMap = {
-    "fldQ56Cbf0pEW2cxx": "fld7HU3A7jWLbuzDU",   // Date of Most Recent Entry -> Date of Last Entry (Asylum Screening)
-}
+    fldQ56Cbf0pEW2cxx: "fld7HU3A7jWLbuzDU", // Date of Most Recent Entry -> Date of Last Entry (Asylum Screening)
+};
 
-const singleChoiceFieldMap = {  // map from field IDs in Asylum Screening Form table to All Contacts table
-    "fld7cZumyeYNTDWqt": "fldzxUADCkam6dLCR",   // How Entered US -> How Entered US (Asylum Screening)
-    "fld3ZrmO7G6JhWtFm": "fldcuQYP0Fmb1SLwq",   // Current Immigration Status -> Current Immigration Status (Asylum Screening)
-    "fldnd6G26IWb5hpx8": "fldzEWOEGf2hcD9Rt",   // Arrested in U.S.? -> Arrested in U.S. (Asylum Screening)
-    "fldAMTgJzSF85YiIo": "fldwdXJ1MTAPuWHnz",   // Spoken to Lawyer? -> Spoken to Lawyer (Asylum Screening)
-    "fldMptekewPll9U2a": "fld5g5l6x1qBli3HF",   // Spoken to Oasis before -> Prior Contact with Oasis (Asylum Screening)
-    "fldIArklsIUvTcQJC": "fldXEd7mML9XRoBaj",   // Already Applied for Asylum? -> Already Filed I-589? (Asylum Screening)
-}
+// noinspection SpellCheckingInspection
+const singleChoiceFieldMap = {
+    // map from field IDs in Asylum Screening Form table to All Contacts table
+    fld7cZumyeYNTDWqt: "fldzxUADCkam6dLCR", // How Entered US -> How Entered US (Asylum Screening)
+    fld3ZrmO7G6JhWtFm: "fldcuQYP0Fmb1SLwq", // Current Immigration Status -> Current Immigration Status (Asylum Screening)
+    fldnd6G26IWb5hpx8: "fldzEWOEGf2hcD9Rt", // Arrested in U.S.? -> Arrested in U.S. (Asylum Screening)
+    fldAMTgJzSF85YiIo: "fldwdXJ1MTAPuWHnz", // Spoken to Lawyer? -> Spoken to Lawyer (Asylum Screening)
+    fldMptekewPll9U2a: "fld5g5l6x1qBli3HF", // Spoken to Oasis before -> Prior Contact with Oasis (Asylum Screening)
+    fldIArklsIUvTcQJC: "fldXEd7mML9XRoBaj", // Already Applied for Asylum? -> Already Filed I-589? (Asylum Screening)
+};
 
-const multiChoiceFieldMap = {    // map from field IDs in Asylum Screening Form table to All Contacts table
-    "fldU3PLanlmM5yXDl": "fldRrH5d7z3uTxdQD",   // Pronouns -> Pronouns
-}
+// noinspection SpellCheckingInspection
+const multiChoiceFieldMap = {
+    // map from field IDs in Asylum Screening Form table to All Contacts table
+    fldU3PLanlmM5yXDl: "fldRrH5d7z3uTxdQD", // Pronouns -> Pronouns
+};
 
-const attachmentFieldMap = {  // map from field IDs in Asylum Screening Form table to All Contacts table
-    "fldkIVOk69sSm4FAL": "fldtWTfL7aLfTjMLd",   // Documents -> Documents Provided
-}
+// noinspection SpellCheckingInspection
+const attachmentFieldMap = {
+    // map from field IDs in Asylum Screening Form table to All Contacts table
+    fldkIVOk69sSm4FAL: "fldtWTfL7aLfTjMLd", // Documents -> Documents Provided
+};
 
 // Script invocation (as automation)
-const formTable = base.getTable("tblEY7Yv4aiJxVSbf")    // Asylum Screening Form Table
-const masterTable = base.getTable("tblsnJnJ4ubpZFLwq")  // All Contacts Master Table
-const { formRecordId } = input.config()
-const formRecord = await formTable.selectRecordAsync(formRecordId)
+// noinspection SpellCheckingInspection
+const formTable = base.getTable("tblEY7Yv4aiJxVSbf"); // Asylum Screening Form Table
+// noinspection SpellCheckingInspection
+const masterTable = base.getTable("tblsnJnJ4ubpZFLwq"); // All Contacts Master Table
+const { formRecordId } = input.config();
+const formRecord = await formTable.selectRecordAsync(formRecordId);
 if (!formRecord) {
-    throw new Error(`No asylum screening form record exists for ID ${formRecordId}`)
+    throw new Error(`No asylum screening form record exists for ID ${formRecordId}`);
 }
-const { masterRecordId } = await extractMatchData()
-await newAsylumScreeningRecordAction()
+const { masterRecordId } = await extractMatchData();
+await newAsylumScreeningRecordAction();
 
 async function extractMatchData() {
-    const masterLinks = formRecord.getCellValue(formNamedFieldMap.masterLink) || []
+    const masterLinks = formRecord.getCellValue(formNamedFieldMap.masterLink) || [];
     if (!masterLinks.length) {
-        throw new Error(`No master record ID in incoming asylum screening form ${formRecordId}!`)
+        throw new Error(`No master record ID in incoming asylum screening form ${formRecordId}!`);
     }
-    const masterRecordId = masterLinks[0].id
+    const masterRecordId = masterLinks[0].id;
     // console.log(JSON.stringify({ masterRecordId }, null, 2))
-    return { masterRecordId }
+    return { masterRecordId };
 }
 
 async function newAsylumScreeningRecordAction() {
     // if this form refers to a specific record, update that one
     let masterRecord = await masterTable.selectRecordAsync(masterRecordId);
     if (!masterRecord) {
-        throw new Error(`Form refers to master record ${masterRecordId}, but it doesn't exist`)
+        throw new Error(`Form refers to master record ${masterRecordId}, but it doesn't exist`);
     }
-    const hook = "https://hook.us1.make.com/h61316aga8y1rjfwkweqkz6rpjqtbo81"
-    const linkId = masterRecord.getCellValue(masterNamedFieldMap.linkId)
+    const hook = "https://hook.us1.make.com/h61316aga8y1rjfwkweqkz6rpjqtbo81";
+    const linkId = masterRecord.getCellValue(masterNamedFieldMap.linkId);
     if (linkId) {
-        await triggerLinkUpdate(hook, masterRecordId, linkId)
+        await triggerLinkUpdate(hook, masterRecordId, linkId);
     } else {
-        console.warn(`Form refers to master record ${masterRecordId}, but it has no link to update`)
+        console.warn(
+            `Form refers to master record ${masterRecordId}, but it has no link to update`
+        );
     }
-    await updateExistingMasterRecord(masterRecord)
+    await updateExistingMasterRecord(masterRecord);
 }
 
 async function updateExistingMasterRecord(masterRecord) {
     // a form has been submitted against this record
-    const masterFields = { [masterNamedFieldMap.link]: "Submitted" }
-    let conflicts = ""
+    const masterFields = { [masterNamedFieldMap.link]: "Submitted" };
+    let conflicts = "";
     for (let [srcKey, targetKey] of Object.entries(stringFieldMap)) {
-        const master = masterRecord.getCellValueAsString(targetKey)
-        const form = formRecord.getCellValueAsString(srcKey)
+        const master = masterRecord.getCellValueAsString(targetKey);
+        const form = formRecord.getCellValueAsString(srcKey);
         if (!form || master === form) {
         } else if (!master) {
-            masterFields[targetKey] = form
+            masterFields[targetKey] = form;
         } else {
-            const fieldName = masterTable.getField(targetKey).name
-            conflicts += `\t${fieldName}: ${form.replace(/\n/g,' ')}\n`
+            const fieldName = masterTable.getField(targetKey).name;
+            conflicts += `\t${fieldName}: ${form.replace(/\n/g, " ")}\n`;
         }
     }
     for (let [srcKey, targetKey] of Object.entries(dateFieldMap)) {
-        const master = masterRecord.getCellValue(targetKey) || ''
-        const form = formRecord.getCellValue(srcKey) || ''
+        const master = masterRecord.getCellValue(targetKey) || "";
+        const form = formRecord.getCellValue(srcKey) || "";
         if (!form || master === form) {
         } else if (!master) {
-            masterFields[targetKey] = form
+            masterFields[targetKey] = form;
         } else {
-            const fieldName = masterTable.getField(targetKey).name
-            conflicts += `\t${fieldName}: ${form}\n`
+            const fieldName = masterTable.getField(targetKey).name;
+            conflicts += `\t${fieldName}: ${form}\n`;
         }
     }
     for (let [srcKey, targetKey] of Object.entries(singleChoiceFieldMap)) {
-        const master = masterRecord.getCellValueAsString(targetKey)
-        const form = formRecord.getCellValueAsString(srcKey)
+        const master = masterRecord.getCellValueAsString(targetKey);
+        const form = formRecord.getCellValueAsString(srcKey);
         if (!form || master === form) {
         } else if (!master) {
-            masterFields[targetKey] = { name: form }
+            masterFields[targetKey] = { name: form };
         } else {
-            const fieldName = masterTable.getField(targetKey).name
-            conflicts += `\t${fieldName}: ${form}\n`
+            const fieldName = masterTable.getField(targetKey).name;
+            conflicts += `\t${fieldName}: ${form}\n`;
         }
     }
     for (let [srcKey, targetKey] of Object.entries(multiChoiceFieldMap)) {
-        const master = masterRecord.getCellValue(targetKey) || []
-        const masterVals = master.map(v => v.name).sort().join("|")
-        const form = formRecord.getCellValue(srcKey) || []
-        const formVals = form.map(v => v.name).sort().join("|")
+        const master = masterRecord.getCellValue(targetKey) || [];
+        const masterVals = master
+            .map((v) => v.name)
+            .sort()
+            .join("|");
+        const form = formRecord.getCellValue(srcKey) || [];
+        const formVals = form
+            .map((v) => v.name)
+            .sort()
+            .join("|");
         if (!formVals || masterVals === formVals) {
         } else if (!master.length) {
-            masterFields[targetKey] = form.map(v => ({ name: v.name }))
+            masterFields[targetKey] = form.map((v) => ({ name: v.name }));
         } else {
-            const fieldName = masterTable.getField(targetKey).name
-            const val = form.map(v => v.name).join(', ')
-            conflicts += `\t${fieldName}: ${val}\n`
+            const fieldName = masterTable.getField(targetKey).name;
+            const val = form.map((v) => v.name).join(", ");
+            conflicts += `\t${fieldName}: ${val}\n`;
         }
     }
     for (let [srcKey, targetKey] of Object.entries(attachmentFieldMap)) {
-        const master = masterRecord.getCellValue(targetKey) || []
-        const form = formRecord.getCellValue(srcKey) || []
+        const master = masterRecord.getCellValue(targetKey) || [];
+        const form = formRecord.getCellValue(srcKey) || [];
         if (form.length) {
             // console.log(`Sending attachments value: ${JSON.stringify(form, null, 2)}`)
-            masterFields[targetKey] = [...master, ...form]
+            masterFields[targetKey] = [...master, ...form];
         }
     }
     if (conflicts) {
-        const master = masterRecord.getCellValueAsString(masterNamedFieldMap.conflicts)
-        const date = new Date(formRecord.getCellValue(formNamedFieldMap.createdDate))
-        const timestamp = date.toLocaleString("en-US", { timeZone: "America/Los_Angeles" })
-        const header = `Additional asylum screening data submitted ${timestamp}:`
-        masterFields[masterNamedFieldMap.conflicts] = `${header}\n${conflicts}\n${master}`
+        const master = masterRecord.getCellValueAsString(masterNamedFieldMap.conflicts);
+        const date = new Date(formRecord.getCellValue(formNamedFieldMap.createdDate));
+        const timestamp = date.toLocaleString("en-US", { timeZone: "America/Los_Angeles" });
+        const header = `Additional asylum screening data submitted ${timestamp}:`;
+        masterFields[masterNamedFieldMap.conflicts] = `${header}\n${conflicts}\n${master}`;
     }
-    await masterTable.updateRecordAsync(masterRecord.id, masterFields)
+    await masterTable.updateRecordAsync(masterRecord.id, masterFields);
 }
 
 async function triggerLinkUpdate(hook, recordId, linkId) {
     console.log(`Requesting reset of link '${linkId}' from record ${recordId}...`);
     const response = await fetch(hook, {
-        method: 'POST',
+        method: "POST",
         headers: {
-            'Content-Type': 'application/json',
+            "Content-Type": "application/json",
         },
         body: JSON.stringify({
             recordId: recordId,
-            shortLinkId: linkId
-        })
+            shortLinkId: linkId,
+        }),
     }).catch((error) => {
         console.error("Error sending request to make:", error);
-    })
+    });
     if (response) {
         console.log(`Response code from make is ${response.status}`);
     }
--- airtable/scripts/library/airtable.internal.d.ts
+++ airtable/scripts/library/airtable.internal.d.ts
@@ -2,76 +2,77 @@
 // such as automations and extensions.
 
 export interface Input {
-    config(): { [key: string]: any },
+    config(): { [key: string]: any };
 }
 // noinspection JSUnusedGlobalSymbols
-export const input: Input
+export const input: Input;
 
 export interface Output {
-    text(text: string): void,
+    text(text: string): void;
 }
 // noinspection JSUnusedGlobalSymbols
-export const output: Output
+export const output: Output;
 
 export interface Base {
-    getTable(idOrName: string): Table,
+    getTable(idOrName: string): Table;
 }
 // noinspection JSUnusedGlobalSymbols
-export const base: Base
+export const base: Base;
 
-export function remoteFetchAsync(url: string, options: RequestInit): Promise<Response>
+export function remoteFetchAsync(url: string, options: RequestInit): Promise<Response>;
 
 export interface Table {
-    id: string,
-    name: string,
-    description?: string,
-    url: string,
-    fields: Field[],
+    id: string;
+    name: string;
+    description?: string;
+    url: string;
+    fields: Field[];
+    getField(idOrName: string): Field | undefined;
     selectRecordAsync(
         recordId: string,
         options?: {
-            fields?: Array<Field | string>,
-        },
-    ): Promise<Record | null>
+            fields?: Array<Field | string>;
+        }
+    ): Promise<Record | null>;
     selectRecordsAsync(options?: {
         sorts?: Array<{
-            field: Field | string,
-            direction?: 'asc' | 'desc',
-        }>,
-        fields?: Array<Field | string>,
-        recordIds?: Array<string>,
-    }): Promise<RecordQueryResult>
-    createRecordAsync(
-        fields: {[fieldNameOrId: string]: unknown}
-    ): Promise<string>
+            field: Field | string;
+            direction?: "asc" | "desc";
+        }>;
+        fields?: Array<Field | string>;
+        recordIds?: Array<string>;
+    }): Promise<RecordQueryResult>;
+    createRecordAsync(fields: { [fieldNameOrId: string]: unknown }): Promise<string>;
     createRecordsAsync(
-        records: Array<{fields: {[fieldNameOrId: string]: unknown}}>
-    ): Promise<Array<string>>
+        records: Array<{ fields: { [fieldNameOrId: string]: unknown } }>
+    ): Promise<Array<string>>;
     updateRecordAsync(
         recordOrRecordId: Record | string,
-        fields: {[fieldNameOrId: string]: unknown}
-    ): Promise<void>
-    updateRecordsAsync(records: Array<{
-        id: string,
-        fields: {[fieldNameOrId: string]: unknown},
-    }>): Promise<void>
+        fields: { [fieldNameOrId: string]: unknown }
+    ): Promise<void>;
+    updateRecordsAsync(
+        records: Array<{
+            id: string;
+            fields: { [fieldNameOrId: string]: unknown };
+        }>
+    ): Promise<void>;
 }
 
 export interface Field {
-    id: string,
-    name: string,
-    description?: string,
-    type: string,
+    id: string;
+    name: string;
+    description?: string;
+    type: string;
 }
 
 export interface Record {
-    id: string,
-    name: string,
-    getCellValue(idOrName: string): any
-    getCellValueAsString(idOrName: string): string
+    id: string;
+    name: string;
+    getCellValue(idOrName: string): any;
+    getCellValueAsString(idOrName: string): string;
 }
 
 export interface RecordQueryResult {
-    recordIds: string[],
-    records: Record[],
+    recordIds: string[];
+    records: Record[];
 }
